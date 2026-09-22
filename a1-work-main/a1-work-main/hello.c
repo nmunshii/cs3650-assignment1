04408d6deb5f7361084c9784707c5f2cd9b3242c
nmunshii <munshinadim55@gmail.com>
2026-09-21 22:09:28 -0400

    added for loop

--- a1-work-main/a1-work-main/hello.c
+++ a1-work-main/a1-work-main/hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Munshi\nmunshinadim55@gmail.com\n");
-
+  for(int i = 0; i < 20; i++) {
+    printf("Munshi\nmunshinadim55@gmail.com\n");
+  }
   return 0;
 }
